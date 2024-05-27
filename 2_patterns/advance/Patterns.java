7744a0690b2e35b1a8f659dbd5c289138d524116
Yash Vijay Bombale <yashbombale@gmail.com>
2024-05-27 20:44:32 +0530

    Complete floyds triangle

--- 2_patterns/advance/Patterns.java
+++ 2_patterns/advance/Patterns.java
@@ -35,8 +35,37 @@ public class Patterns {
   }
 
 
+  //inverted half pyramid with numbers;
+  public static void inverted_Reverse_Half_Pyramid(int n){
+        
+            
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            System.out.print(j+" ");
+        }
+        
+        System.out.println();
+    }
+    
+}
+
+
+    public static void floydsTriangle(int n){
+      int counter=1;
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=i;j++){
+              System.out.print(counter+" ");
+              counter++;
+          }
+          System.out.println();
+      }
+    }
+
   public static void main(String[] args) {
     hollowRectangle(5, 15);
     invertedHalfPyramid(5);
+    inverted_Reverse_Half_Pyramid(20);
+    System.out.println("Floyds triangle");
+    floydsTriangle(5);
   }
 }
