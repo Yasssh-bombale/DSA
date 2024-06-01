39387d1f82c7dd889740f12f092cbf1eb3f2b440
Yash Vijay Bombale <yashbombale@gmail.com>
2024-06-01 11:53:00 +0530

    Complete advance patterns

--- 2_patterns/advance/Patterns.java
+++ 2_patterns/advance/Patterns.java
@@ -61,11 +61,128 @@ public class Patterns {
       }
     }
 
+    public static void zero_One_Half_Triangle(){
+            
+      System.out.println("Zero one half triangle pattern");
+      for(int i=1;i<=5;i++){
+          //innner logic;
+         for(int j=1;j<=i;j++){
+             if((i+j)%2==0){
+                 System.out.print("1");
+             }else{
+                 System.out.print("0");
+             }
+         }
+         System.out.println();
+      }    
+      
+  }
+
+  public static void butterfly_pattern(int n){
+    System.out.println("\n Butterfly pattern pattern \n");
+    for(int i=1;i<=n;i++){
+        //stars;
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        //spaces;
+        for(int j=1;j<=2*(n-i);j++){
+            System.out.print(" ");
+        }
+        
+        //reverse;
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        
+        System.out.println(); 
+    }
+    
+    // mirror image / copy of previous half;
+    for(int i=n;i>=1;i--){
+         //stars;
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        //spaces;
+        for(int j=1;j<=2*(n-i);j++){
+            System.out.print(" ");
+        }
+        
+        //reverse;
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        
+        System.out.println(); 
+    }
+    
+}
+
+
+public static void solid_Rhombus(int n){
+
+  System.out.println("\n Solid Rhombus pattern \n");
+
+
+  for(int i=1;i<=n;i++){
+      //spaces;
+      for(int j=1;j<=n-i;j++){
+          System.out.print(" ");
+      }
+      //stars;
+      for(int j=1;j<=n;j++){
+          System.out.print("*"+" ");
+      }
+      // spaces;
+      for(int j=1;j<=i;j++){
+          System.out.print(" ");
+      }
+      
+      System.out.println();
+  }
+}
+public static void diamond_pattern(int n){
+  
+  System.out.println("\n Diamond pattern \n");
+  for(int i=1;i<=n;i++){
+      //spaces;
+      for(int j=1;j<=n-i;j++){
+          System.out.print(" ");
+      }
+      
+      //stars;
+      for(int j=1;j<=(2 *i)-1;j++){
+          System.out.print("*");
+      }
+      
+      System.out.println();
+  }
+  //mirror image;
+   for(int i=n;i>=1;i--){
+           //spaces;
+      for(int j=1;j<=n-i;j++){
+          System.out.print(" ");
+      }
+      
+      //stars;
+      for(int j=1;j<=(2 *i)-1;j++){
+          System.out.print("*");
+      }
+      
+      System.out.println();
+   }
+  
+}
   public static void main(String[] args) {
     hollowRectangle(5, 15);
     invertedHalfPyramid(5);
     inverted_Reverse_Half_Pyramid(20);
     System.out.println("Floyds triangle");
     floydsTriangle(5);
+    zero_One_Half_Triangle();
+    butterfly_pattern(8);
+    solid_Rhombus(10);
+    diamond_pattern(5);
   }
 }
