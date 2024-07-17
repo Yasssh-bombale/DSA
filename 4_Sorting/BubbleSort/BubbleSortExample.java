9c371647bfbeab42bd96fe8de014982a20774f21
Yash Vijay Bombale <yashbombale@gmail.com>
2024-07-17 18:14:20 +0530

    Complete bubbleSort

---
+++ 4_Sorting/BubbleSort/BubbleSortExample.java
@@ -0,0 +1,28 @@
+
+
+class BubbleSortExample {
+        
+  public static void bubbleSort(int arr[]){
+      for(int turn=0;turn<arr.length-1;turn++){
+          for(int j=0;j<arr.length-1-turn;j++){
+              if(arr[j]>arr[j+1]){
+                  //swap arr[j] with arr[j+1];
+                  int temp = arr[j+1];
+                  arr[j+1]=arr[j];
+                  arr[j]=temp;
+              }
+          }
+      }
+  }
+  
+  public static void main(String[] args) {
+      int arr[]={5,4,1,3,2};
+      bubbleSort(arr);
+      
+      for(int i=0;i<arr.length;i++){
+          System.out.print(arr[i]+" ");
+      }
+      System.out.println();
+      
+  }
+}
